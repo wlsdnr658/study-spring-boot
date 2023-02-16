5d3e4d5fa5b4c0c29e3f266b36aa9a7afac1c526
wlsdnr658 <wlsdnr658@gmail.com>
2023-02-16 10:07:04 +0900

    commit

--- src/main/java/kr/co/study/controller/StudyController.java
+++ src/main/java/kr/co/study/controller/StudyController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping(value = "/study")
+@RequestMapping(value = "/study") 
 public class StudyController {
 	
 	@GetMapping(value = "")
